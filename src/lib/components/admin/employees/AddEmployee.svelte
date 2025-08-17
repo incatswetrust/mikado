b5ca40eb056dbe898b98ec0fe14ae5f6a1e9be47
Oleksii <cmmxr777@gmail.com>
2025-08-17 22:33:44 +0300

    fixed adding new employee

--- src/lib/components/admin/employees/AddEmployee.svelte
+++ src/lib/components/admin/employees/AddEmployee.svelte
@@ -88,19 +88,18 @@
                     <div>
                         <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
                         <select bind:value={form.status} id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option selected>Work</option>
-                            <option value="TV">On vacations</option>
-                            <option value="PC">Fired</option>
+                            <option selected value="active">Active</option>
+                            <option value="inactive">Inactive</option>
+                            <option value="on_leave">On leave</option>
                         </select>
                     </div>
                     <div>
                         <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
                         <select bind:value={form.category} id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option selected>Select category</option>
-                            <option value="TV">TV/Monitors</option>
-                            <option value="PC">PC</option>
-                            <option value="GA">Gaming/Console</option>
-                            <option value="PH">Phones</option>
+                            <option selected value="admin">Administrator</option>
+                            <option value="nails">Nail master</option>
+                            <option value="hairdresser">Hairdresser</option>
+                            <option value="another">Another</option>
                         </select>
                     </div>
                     <div>
--- src/lib/components/admin/employees/EmployeeTableUnit.svelte
+++ src/lib/components/admin/employees/EmployeeTableUnit.svelte
@@ -16,7 +16,7 @@
     <td class="px-4 py-3">{employee.phone}</td>
     <td class="px-4 py-3">{employee.hire_date}</td>
     <td class="px-4 py-3 flex items-center justify-end">
-        <button id={triggerId} data-dropdown-toggle={dropdownId} class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
+        <button aria-label="dropdown-button" id={triggerId} data-dropdown-toggle={dropdownId} class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
             <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
             </svg>
